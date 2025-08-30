7c0b2477d6352f74fbd9484fc89e3b8378d2dd0c
Jhon Garcia <>
2025-08-30 12:31:34 -0500

    [IMP] portcities_construction_site_mgmt: rename Project to Site and Task to Milestone
    Implement Case 1 requirements 1 and 2:
    - Change all labels 'Project/s' in the project module to 'Site/s' (menus, field labels)
    - Change all labels 'Task/s' in the project module to 'Milestone/s' (menus, field labels)

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Port Cities Case 1 - Site & Milestone Customization',
+    'version': '16.0.1.0.0',
+    'category': 'Project',
+    'summary': 'Port Cities Case 1: Customize Project module to use Site and Milestone terminology',
+    'description': """
+        Port Cities Case Study 1 - Construction Site Management
+        
+        This module customizes the Project module for construction companies:
+        - Change 'Project' labels to 'Site' (construction sites)
+        - Change 'Task' labels to 'Milestone' (project milestones)
+
+        
+        Developed as part of Port Cities software engineer assessment.
+    """,
+    'author': 'Jhon Garcia',
+    'maintainer': 'Jhon Garcia',
+    'depends': ['project'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
